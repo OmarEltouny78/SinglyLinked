920df895e3707e42eeccfe8e214251403358a49e
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2022-10-11 11:02:26 +0200

    Add files via upload

---
+++ SinglyLinkedListTemp.java
@@ -0,0 +1,37 @@
+public class SinglyLinkedLIst<E> {
+
+
+    private static class Node<E>{
+
+    }
+
+    public void insertNth(int position,Node head, E data){
+        
+    }
+
+    public SinglyLinkedLIst() {
+    }
+
+    public int getSize() {
+        return 0;
+    }
+    public boolean isEmpty(){
+        return false;
+    }
+    public E first(){
+        return null;
+    }
+    public E last(){
+        return null;
+    }
+    public void addFirst(E e){
+        
+    }
+    public void addLast(E e){
+        
+    }
+    public E removeFirst(){
+        return null;
+    }
+    
+}
